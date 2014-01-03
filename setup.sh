de1c5b7b5845f3b50be1ba85324cad750e0d7d73
Timoshenko <timoshenko@softctrlcom.local>
2014-01-02 21:04:13 -0300

    some changes to make this right

--- setup.sh
+++ setup.sh
@@ -1,15 +1,11 @@
 # Install many packages needed
 yum -y upgrade
 yum -y groupinstall "Development Tools"
-yum -y install wget ncurses* openssl* libXt* glib* libjpeg* libpng* libxml* libxslt* libtiff* libXext* gnutls* gcc-objc* libobjc*
+yum -y install wget ncurses* openssl* libXt* glib* libjpeg* libpng* libxml* libxslt* libtiff* libXext* gnutls* gcc-objc* libobjc* libffi*
 
-# Install FFI support
-wget -c "ftp://sourceware.org/pub/libffi/libffi-3.0.13.tar.gz" && tar -zxvf libffi-3.0.13.tar.gz && cd libffi-3.0.13 && ./configure && make && make install && cd .. && rm -rvf libffi-3.0.13/
-cp -Rvf /usr/local/lib64/libffi.* /usr/lib64/
+# Install FFI supportwget -c "ftp://sourceware.org/pub/libffi/libffi-3.0.13.tar.gz" && tar -zxvf libffi-3.0.13.tar.gz && cd libffi-3.0.13 && ./configure --prefix=/usr/include && make && make install && cd .. && rm -rvf libffi-3.0.13/ && cp -Rvf /usr/include/lib64/libffi* /usr/lib64/
 
-# Install ICU support
-wget -c "http://download.icu-project.org/files/icu4c/52rc/icu4c-52_rc2-src.tgz" && tar -zxvf icu4c-52_rc2-src.tgz && cd icu/source && ./configure  && gmake && gmake install && cd ../.. && rm -rvf icu/
-cp -Rvf /usr/local/lib/libicu* /usr/lib64/
+# Install ICU supportwget -c "http://download.icu-project.org/files/icu4c/52rc/icu4c-52_rc2-src.tgz" && tar -zxvf icu4c-52_rc2-src.tgz && cd icu/source && ./configure --prefix=/usr/include && gmake && gmake install && cd ../.. && rm -rvf icu/
 
 # Install GNUStep Startup
 wget -c "ftp://ftp.gnustep.org/pub/gnustep/core/gnustep-startup-0.32.0.tar.gz" && tar -zxvf gnustep-startup-0.32.0.tar.gz && cd gnustep-startup-0.32.0 && ./InstallGNUstep && cd .. && rm -rvf gnustep-startup-0.32.0/
@@ -17,11 +13,9 @@ wget -c "ftp://ftp.gnustep.org/pub/gnustep/core/gnustep-startup-0.32.0.tar.gz" &
 # Install GNUStep Make
 wget -c "ftp://ftp.gnustep.org/pub/gnustep/core/gnustep-make-2.6.6.tar.gz" && tar -zxvf gnustep-make-2.6.6.tar.gz && cd gnustep-make-2.6.6 && ./configure && make && make install && cd .. && rm -rvf gnustep-make-2.6.6
 
-# Install GNUStep Base
-wget -c "ftp://ftp.gnustep.org/pub/gnustep/core/gnustep-base-1.24.6.tar.gz" && tar -zxvf gnustep-base-1.24.6.tar.gz && cd gnustep-base-1.24.6 && ./configure && make && make install && cd .. && rm -rvf gnustep-base-1.24.6/
-cp -Rvf /usr/local/lib/libgnustep-base* /usr/lib64/
+# Install GNUStep Basewget -c "ftp://ftp.gnustep.org/pub/gnustep/core/gnustep-base-1.24.6.tar.gz" && tar -zxvf gnustep-base-1.24.6.tar.gz && cd gnustep-base-1.24.6 && ./configure --prefix=/usr/include && make && make install && cd .. && rm -rvf gnustep-base-1.24.6/ 
 
-# Install GNUStep GUI 
+# Install GNUStep GUI
 wget -c "ftp://ftp.gnustep.org/pub/gnustep/core/gnustep-gui-0.24.0.tar.gz" && tar -zxvf gnustep-gui-0.24.0.tar.gz && cd gnustep-gui-0.24.0 && ./configure && make && make install && cd .. && rm -rvf gnustep-gui-0.24.0/
 
 # Install GNUStep Back
